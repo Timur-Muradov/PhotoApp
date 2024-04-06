6feee0d0d4ae416770012c24b0e5c91111607b2f
Timur-Muradov <timur.muradov.1996@mail.ru>
2024-04-06 14:36:21 +0300

    Initial Commit

---
+++ PhotoApp/PhotoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoAppApp.swift
+//  PhotoApp
+//
+//  Created by Тимур Мурадов on 06.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct PhotoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
